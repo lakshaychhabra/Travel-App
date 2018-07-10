ef4f1fd6f8da0e1f76e7196f2b9b5a115238c51a
lakshaychhabra <lakshay.chhabra@hotmail.com>
2018-07-10 17:31:41 +0530

    Added Modal and Functions

---
+++ Travel App/Modals/Trip/TripFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  TripFunctions.swift
+//  Travel App
+//
+//  Created by Lakshay Chhabra on 10/07/18.
+//  Copyright © 2018 Lakshay Chhabra. All rights reserved.
+//
+
+import Foundation
+
+
+class TripFunctions {
+    
+    static func createTrip(tripModel : TripModal){
+        
+    }
+    static func readTrip(){
+        
+    }
+    static func updateTrip(tripModel : TripModal){
+        
+    }
+    static func deleteTrip(tripModel : TripModal){
+        
+    }
+    
+}
